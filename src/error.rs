4583edc4ef84fffdc45d8b9878e7206296346bfb
Matthias Beyer <mail@beyermatthias.de>
2018-03-15 16:04:59 +0100

    Refactor error handling

--- src/error.rs
+++ src/error.rs
@@ -9,6 +9,8 @@ error_chain! {
 
     foreign_links {
         Io(::std::io::Error);
+        HandlebarsRenderError(::handlebars::RenderError);
+        HandlebarsFileError(::handlebars::TemplateError);
     }
 
     errors {
--- src/handlers/issue.rs
+++ src/handlers/issue.rs
@@ -8,45 +8,42 @@ use handlebars::Handlebars;
 use libgitdit::RepositoryExt;
 use libgitdit::Message;
 
+use error::*;
+use error::GitDitWuiError as GDWE;
 use middleware::repository::RepositoryMiddlewareData;
 use middleware::handlebars::HandlebarsMiddlewareData;
 use renderer::types::issue::IssueListItem;
 
 pub fn index(mut state: State) -> (State, Response) {
-    let output = {
-        let repo      = RepositoryMiddlewareData::borrow_mut_from(&mut state).repo();
-        let repo_lock = repo.lock().unwrap();
+    let repo      = RepositoryMiddlewareData::borrow_mut_from(&mut state).repo();
+    let repo_lock = repo.lock().unwrap();
 
-        let hb      = HandlebarsMiddlewareData::borrow_from(&mut state).handlebars();
-        let hb_lock = hb.lock().unwrap();
+    let hb      = HandlebarsMiddlewareData::borrow_from(&mut state).handlebars();
+    let hb_lock = hb.lock().unwrap();
 
-        let data      = match repo_lock.issues() {
-            Ok(issues) => {
-                let data = {
-                    let i = issues
-                        .iter()
-                        .map(IssueListItem::from_issue)
-                        .map(Result::unwrap)
-                        .collect::<Vec<IssueListItem>>();
-                    let mut b = ::std::collections::BTreeMap::new();
-                    b.insert("issues", i);
-                    b
-                };
-
-                hb_lock.render("issue_list", &data).unwrap()
-            },
-
-            Err(e) => {
-                String::from("Failed to get issues")
-            }
-        };
-
-        data.into_bytes()
-    };
+    let (output, statuscode) = repo_lock
+        .issues()
+        .map_err(GDWE::from)
+        .and_then(|issues| {
+            issues.iter()
+                .map(IssueListItem::from_issue)
+                .collect::<Result<Vec<IssueListItem>>>()
+                .and_then(|issues| {
+                    let mut data = ::std::collections::BTreeMap::new();
+                    data.insert("issues", issues);
+                    hb_lock.render("issue_list", &data)
+                        .map_err(GDWE::from)
+                })
+                .map(|s| s.into_bytes())
+        })
+        .map(|x| (x, StatusCode::Ok))
+        .unwrap_or_else(|e| {
+            (format!("Error: {:?}", e).into_bytes(), StatusCode::InternalServerError)
+        });
 
     let res = create_response(
         &state,
-        StatusCode::Ok,
+        statuscode,
         Some((output, mime::TEXT_HTML)),
     );
 
@@ -64,3 +61,4 @@ pub fn get_issue_handler(mut state: State) -> (State, Response) {
 
     (state, res)
 }
+
