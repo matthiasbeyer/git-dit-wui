5e0b4de12ae38053c1934d7285ff61e71259d85f
Matthias Beyer <mail@beyermatthias.de>
2018-03-15 23:05:00 +0100

    Include head

--- src/renderer/header.rs
+++ src/renderer/header.rs
@@ -2,6 +2,13 @@ use horrorshow::RenderBox;
 
 pub fn render_header() -> Box<RenderBox> {
     box_html! {
+        head {
+            meta(charset = "UTF-8");
+            meta(name = "viewport", content="width=device-width, initial-scale=1");
+            link(rel = "stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css");
+            script(defer, src = "https://use.fontawesome.com/releases/v5.0.6/js/all.js") {
+            }
+        }
     }
 }
 
