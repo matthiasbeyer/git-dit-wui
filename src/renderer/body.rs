ef88c955c8a195fd1d3714dd7c4b4fc66879b590
Matthias Beyer <mail@beyermatthias.de>
2018-03-15 23:04:55 +0100

    Add header/footer

--- src/renderer/body.rs
+++ src/renderer/body.rs
@@ -2,10 +2,23 @@ use horrorshow::RenderBox;
 
 pub fn render_body_pre() -> Box<RenderBox> {
     box_html! {
+        div(id = "nav") {
+            a(href = "/"): "Home";
+            a(href = "/issues"): "Issues";
+        }
     }
 }
 
 pub fn render_body_post() -> Box<RenderBox> {
     box_html! {
+        div(id = "footer") {
+            div(id = "attribution") {
+                : "Made with Love, the gotham web framework, horrorshow-rs and libgitdit"
+            }
+
+            div(id = "version") {
+                : "Licensed under AGPL-3.0"
+            }
+        }
     }
 }
