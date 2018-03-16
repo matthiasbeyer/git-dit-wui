b4a430fd6eabacb4a721e406fbdc6cf958262c28
Matthias Beyer <mail@beyermatthias.de>
2018-03-16 17:14:06 +0100

    Add helpers for checking whether issue is open or closed

--- src/util.rs
+++ src/util.rs
@@ -25,3 +25,39 @@ pub fn sort_commits_by_time<'a>(mut v: Vec<Commit<'a>>) -> Vec<Commit<'a>> {
     v.sort_by_key(|commit| commit.time());
     v
 }
+
+pub fn issue_is_open<'a>(i: &Issue<'a>) -> Result<bool> {
+    use libgitdit::trailer::accumulation;
+    use libgitdit::trailer::accumulation::Accumulator;
+    use libgitdit::trailer::TrailerValue;
+    use libgitdit::Message;
+
+    let policy  = accumulation::AccumulationPolicy::Latest;
+    let mut acc = accumulation::SingleAccumulator::new("Dit-status".to_owned(), policy);
+
+    let mut trailers = vec![];
+    for message in i.messages()? {
+        let mut message_trailers = message?.trailers().collect();
+        trailers.append(&mut message_trailers);
+    }
+
+    acc.process_all(trailers.into_iter());
+
+
+    if let Some((_, val)) = acc.into_iter().next() {
+        match val {
+            TrailerValue::String(s) => {
+                debug!("Trailer value: '{}'", s);
+                return Ok(s == "OPEN" || s == "open" || s == "Open");
+            },
+            _ => {},
+        }
+    }
+
+    return Ok(false);
+}
+
+pub fn issue_is_closed<'a>(i: &Issue<'a>) -> Result<bool> {
+    return issue_is_open(i).map(|x| !x);
+}
+
