bce4d74d31becb564ee99f8a12ed1d2097b94470
Matthias Beyer <mail@beyermatthias.de>
2018-03-27 13:12:10 +0200

    Add cache aggregation

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -10,6 +10,7 @@ use chrono::NaiveDateTime;
 use libgitdit::RepositoryExt;
 
 use middleware::repository::RepositoryMiddlewareData;
+use middleware::cache::CacheMiddlewareData;
 use error::GitDitWuiErrorKind as GDWEK;
 use error::*;
 
@@ -17,15 +18,21 @@ pub mod issue;
 pub mod message;
 
 pub fn index(mut state: State) -> (State, Response) {
-    let repo = RepositoryMiddlewareData::borrow_mut_from(&mut state).repo();
-    let repo_lock = repo.lock().unwrap();
+    let path = {
+        let repo      = RepositoryMiddlewareData::borrow_mut_from(&mut state).repo();
+        let repo_lock = repo.lock().unwrap();
+        format!("{}", repo_lock.path().to_path_buf().display())
+    };
 
-    let (output, status) = repo_stats(&repo_lock)
-        .and_then(::renderer::index::render_index)
-        .map(|s| (s, StatusCode::Ok))
-        .unwrap_or_else(|e| {
-            (format!("Failed to process main page: {:?}", e), StatusCode::InternalServerError)
-        });
+    let (output, status) = {
+        let cache = CacheMiddlewareData::borrow_mut_from(&mut state);
+        repo_stats(path, &cache)
+            .and_then(::renderer::index::render_index)
+            .map(|s| (s, StatusCode::Ok))
+            .unwrap_or_else(|e| {
+                (format!("Failed to process main page: {:?}", e), StatusCode::InternalServerError)
+            })
+    };
 
     let res = create_response(
         &state,
@@ -41,52 +48,34 @@ pub struct Stats {
     pub total_number_issues    : usize,
     pub total_number_messages  : usize,
     pub date_issue_count_map   : HashMap<NaiveDateTime, usize>,
-    pub date_message_count_map : HashMap<NaiveDateTime, usize>,
     pub authors                : HashSet<String>,
 }
 
-fn repo_stats(r: &::git2::Repository) -> Result<Stats> {
+fn repo_stats(path: String, cache: &CacheMiddlewareData) -> Result<Stats> {
     let mut total_number_issues    = 0;
     let mut total_number_messages  = 0;
     let mut date_issue_count_map   = HashMap::new();
-    let mut date_message_count_map = HashMap::new();
     let mut authors                = HashSet::new();
 
-    for issue in r.issues()? {
-        total_number_issues += 1;
+    for issue in cache.issues() {
+        total_number_issues   += 1;
+        total_number_messages += issue.number_of_messages();
+
         {
-            let created = time_for_commit(&issue.initial_message()?)?;
+            let created = issue.date().clone();
             let count   = date_issue_count_map.entry(created).or_insert(1);
             *count += 1;
         }
 
-        for message in issue.messages()? {
-            let message = message?;
-            total_number_messages += 1;
-            {
-                let created = time_for_commit(&message)?;
-                let count   = date_message_count_map.entry(created).or_insert(1);
-                *count += 1;
-            }
-
-            let author = message.author();
-            if let Some(name) = author.name().map(String::from) {
-                let _ = authors.insert(name);
-            }
-        }
+        authors.insert(issue.author_name().clone());
     }
 
     Ok(Stats {
-        repo_path              : format!("{}", r.path().to_path_buf().display()),
+        repo_path              : path,
         total_number_issues    : total_number_issues,
         total_number_messages  : total_number_messages,
         date_issue_count_map   : date_issue_count_map,
-        date_message_count_map : date_message_count_map,
         authors                : authors,
     })
 }
 
-fn time_for_commit<'a>(c: &::git2::Commit<'a>) -> Result<NaiveDateTime> {
-    NaiveDateTime::from_timestamp_opt(c.time().seconds(), 0)
-        .ok_or_else(|| GDWEK::NoInitialTimeForIssue(format!("{}", c.id())).into())
-}
